69ea54f44d3ef8ba1fbab08a39483a1c34f031c8
andersnauman <anders.nauman@gmail.com>
2017-03-19 21:11:50 +0100

    Changed encoding

--- DLL/MainProgram.cs
+++ DLL/MainProgram.cs
@@ -22,7 +22,7 @@ namespace DLL
             CallingConvention = CallingConvention.StdCall)]
         private static extern int AllocConsole();
         private const int STD_OUTPUT_HANDLE = -11;
-        private const int MY_CODE_PAGE = 65001;
+        private const int MY_CODE_PAGE = 437;
         [DllImport("kernel32.dll", SetLastError = true)]
         private static extern bool SetConsoleTitle(String IpConsoleTitle);
         [DllImport("kernel32.dll", SetLastError = true)]
