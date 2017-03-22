2e1226c7921e43371cfd1d5b0a167b09d004c72f
andersnauman <anders.nauman@gmail.com>
2017-03-22 22:11:37 +0100

    Merged all changes from DLL

---
+++ EXE/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EXE
+{
+    public class Main
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Windows PowerShell");
+            Console.WriteLine("Copyright(C) 2016 Microsoft Corporation. All rights reserved.\n");
+            PShell pshell = new PShell();
+            while (true)
+            {
+                Console.Write("PS " + pshell.ExecuteCmd("$(Get-Location).Path").Replace(System.Environment.NewLine, String.Empty) + "> ");
+                string cmd = Console.ReadLine();
+                Console.Write(pshell.ExecuteCmd(cmd));
+            }
+        }
+    }
+}
---
+++ EXE/PShell.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace EXE
+{
+    public class PShell
+    {
+        Runspace runspace;
+        public PShell()
+        {
+            runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+        }
+        public string ExecuteCmd(string cmd)
+        {
+            try
+            {
+                PowerShell ps = PowerShell.Create();
+                ps.Runspace = runspace;
+                StringBuilder stringBuilder = new StringBuilder();
+                using (ps)
+                {
+                    ps.AddScript(cmd);
+                    ps.AddCommand("Out-String");
+                    Collection<PSObject> result = ps.Invoke();
+                    if (ps.Streams.Error.Count != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        foreach (var obj in ps.Streams.Error)
+                        {
+                            stringBuilder.AppendLine(obj.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        foreach (var obj in result)
+                        {
+                            stringBuilder.AppendLine(obj.ToString());
+                        }
+                    }
+                }
+                ps = null;
+                return stringBuilder.ToString();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
